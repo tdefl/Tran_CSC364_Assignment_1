9d3c911071277533bde4c0ba6a7b9cd1e950b515
trade <traderflan@gmail.com>
2024-10-25 15:50:57 -0700

    cleaned up repo files

--- run_routers.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start cmd /k "python C:\Users\trade\Desktop\CSC364\my_repo\Tran_CSC364_Assignment_1\router6.py"
-start cmd /k "python C:\Users\trade\Desktop\CSC364\my_repo\Tran_CSC364_Assignment_1\router5.py"
-start cmd /k "python C:\Users\trade\Desktop\CSC364\my_repo\Tran_CSC364_Assignment_1\router3.py"
-start cmd /k "python C:\Users\trade\Desktop\CSC364\my_repo\Tran_CSC364_Assignment_1\router4.py"
-start cmd /k "python C:\Users\trade\Desktop\CSC364\my_repo\Tran_CSC364_Assignment_1\router2.py"
-start cmd /k "python C:\Users\trade\Desktop\CSC364\my_repo\Tran_CSC364_Assignment_1\router1.py"
